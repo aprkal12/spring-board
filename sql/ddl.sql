5a19a1be00f5ee4184856652225d1d6ad4bea2d3
Kim DongHyun <aprkal12@naver.com>
2025-04-21 23:55:20 +0900

    fix : untracked files

--- sql/ddl.sql
+++
@@ -1,5 +0,0 @@
-create table board.board (
-	id int not null auto_increment,
-    title VARCHAR(45) not null,
-    content text not null,
-    primary key(id));
--- sql/fileupload.sql
+++
@@ -1,3 +0,0 @@
-alter 'board'.'board'
-      add column 'file_name' varchar(150) null after 'content',
-        add column 'file_path' varchar(300) null after 'file_name',
